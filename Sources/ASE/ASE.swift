001478d89ed957c904990dcb6526d7ee3a0f1c24
griffin-stewie <panterathefamilyguy@gmail.com>
2020-03-22 17:05:54 +0900

    overhaul

--- Sources/ASE/ASE.swift
+++ Sources/ASE/ASE.swift
@@ -13,86 +13,7 @@ import Cocoa
 // https://github.com/m99coder/ase2json
 // https://github.com/ramonpoca/ColorTools
 
-private extension Data {
-
-    func readUInt32BE(_ position : Int) -> UInt32 {
-        var blocks : UInt32 = 0
-        (self as NSData).getBytes(&blocks, length: position)
-        return NSSwapBigIntToHost(blocks)
-    }
-
-    func readUInt16BE(_ position : Int) -> UInt16 {
-        var blocks : UInt16 = 0
-        (self as NSData).getBytes(&blocks, length: position)
-        return NSSwapBigShortToHost(blocks)
-    }
-
-    func readUInt32LE(_ position : Int) -> UInt32 {
-        var blocks : UInt32 = 0
-        (self as NSData).getBytes(&blocks, length: position)
-        return NSSwapLittleIntToHost(blocks)
-    }
-
-    func readUInt8(_ position : Int) -> UInt8 {
-        var blocks : UInt8 = 0
-        (self as NSData).getBytes(&blocks, length: position)
-        return blocks
-    }
-}
-
-let SIGNATURE = "ASEF"
-
-enum ColorMode: String {
-    case CM_CMYK = "CMYK"
-    case CM_RGB = "RBG"
-    case CM_LAB = "LAB"
-    case CM_GRAY = "Gray"
-}
-
-enum ColorType: Int {
-    case ct_GLOBAL = 0
-    case ct_SPOT = 1
-    case ct_NORMAL = 2
-}
-
-let MODE_COLOR = 1
-let MODE_GROUP = 2
-
-enum ParseState: Int {
-    case getMode = 1
-    case getLength = 2
-    case getName = 3
-    case getModel = 4
-    case getColor = 5
-    case getType = 6
-}
-
-struct ParseModel {
-    var data: Data
-    var blocks: UInt32
-    var state = ParseState.getMode
-    var mode = MODE_COLOR
-    var position = 12
-    var blockLength: UInt32
-
-    init(_ d: Data) {
-        data = d
-        blocks = data.readUInt32BE(8)
-        blockLength = 0
-    }
-
-    func canGo() -> Bool {
-        return position < data.count
-    }
-
-    mutating func addPosition(_ a: Int) {
-        position += a
-    }
-}
-
-let BT_GROUP_START: UInt16 = 0xc001
-let BT_GROUP_END: UInt16 = 0xc002
-let BT_COLOR_ENTRY: UInt16 = 0x0001
+// MARK: - Public
 
 public struct ASE {
 
@@ -124,13 +45,25 @@ public struct ASE {
     }
 }
 
+// MARK: - Internal
+
 extension ASE {
+
+    enum ColorModel: String {
+        case cmyk = "CMYK"
+        case rgb = "RGB " // DO NOT drop trailing space
+        case lab = "LAB " // DO NOT drop trailing space
+        case gray = "Gray"
+    }
 
     static func parse(url: URL) -> NSColorList? {
         return parse(path: url.path)
     }
 
     static func parse(path: String) -> NSColorList? {
+
+        let SIGNATURE = "ASEF"
+
         let colorList = NSColorList(name: "x")
 
         guard let ASEFileHandle = FileHandle(forReadingAtPath:path) else  {
@@ -163,16 +96,18 @@ extension ASE {
     }
 
     static func readBlock(_ fileHandle :FileHandle, to colorList: NSColorList) {
+        let blockTypeColorEntry: UInt16 = 0x0001
+
         let blockType: UInt16 = fileHandle.readUInt16()!
         let blockLength: UInt32 = fileHandle.readUInt32()!
 
         switch blockType {
-        case BT_COLOR_ENTRY:
+        case blockTypeColorEntry:
             let nameLength: UInt16 = fileHandle.readUInt16()!
             let nameData: Data = fileHandle.readData(ofLength: Int(nameLength * 2))
             var name = String(data: nameData, encoding: String.Encoding.utf16)
             let colorModelData: Data = fileHandle.readData(ofLength: 4)
-            if let colorModel = NSString(data: colorModelData, encoding: String.Encoding.ascii.rawValue) {
+            if let colorModel = String(data: colorModelData, encoding: .ascii) {
                 let color = colorFromModel(colorModel, fileHandle: fileHandle)
                 _ = fileHandle.readUInt16()
 
@@ -190,16 +125,6 @@ extension ASE {
                     name = name?.trimmingCharacters(in: set)
                 }
 
-                var i :Int = 1;
-                var fixedName :String = name!
-                while (colorList.color(withKey: fixedName) != nil) {
-                    i += i
-                    let s = String(format: " %ld", i)
-                    fixedName = name!.appending(s)
-                }
-
-                name = fixedName
-
                 colorList.setColor(color, forKey: name!)
             }
 
@@ -208,30 +133,60 @@ extension ASE {
         }
     }
 
-    static func colorFromModel(_ colorModel: NSString, fileHandle: FileHandle) -> NSColor {
-        switch colorModel {
-        case "RGB ":
+    static func colorFromModel(_ colorModel: String, fileHandle: FileHandle) -> NSColor {
+        guard let model = ColorModel(rawValue: colorModel) else {
+            fatalError("Unkonw Color Model")
+        }
+
+        switch model {
+        case .rgb:
             let red :CGFloat = CGFloat(fileHandle.readFloat()!)
             let green :CGFloat = CGFloat(fileHandle.readFloat()!)
             let blue :CGFloat = CGFloat(fileHandle.readFloat()!)
             let color = NSColor(srgbRed: red, green: green, blue: blue, alpha: 1.0)
             return color
-        case "CMYK":
+        case .cmyk:
             let cyan :CGFloat = CGFloat(fileHandle.readFloat()!)
             let magenta :CGFloat = CGFloat(fileHandle.readFloat()!)
             let yellow :CGFloat = CGFloat(fileHandle.readFloat()!)
             let black :CGFloat = CGFloat(fileHandle.readFloat()!)
             let color = NSColor(deviceCyan: cyan, magenta: magenta, yellow: yellow, black: black, alpha: 1.0).usingColorSpace(NSColorSpace.sRGB)!
             return color
-        case "LAB ":
+        case .lab:
             fatalError("Unsupport Color Model")
-        case "Gray":
+        case .gray:
             let white :CGFloat = CGFloat(fileHandle.readFloat()!)
             let color = NSColor(white: white, alpha: 1.0).usingColorSpace(NSColorSpace.sRGB)!
             return color
-        default:
-            fatalError("Unkonw Color Model")
         }
     }
 }
 
+// MARK: - Helper Extension
+
+private extension Data {
+
+    func readUInt32BE(_ position : Int) -> UInt32 {
+        var blocks : UInt32 = 0
+        (self as NSData).getBytes(&blocks, length: position)
+        return NSSwapBigIntToHost(blocks)
+    }
+
+    func readUInt16BE(_ position : Int) -> UInt16 {
+        var blocks : UInt16 = 0
+        (self as NSData).getBytes(&blocks, length: position)
+        return NSSwapBigShortToHost(blocks)
+    }
+
+    func readUInt32LE(_ position : Int) -> UInt32 {
+        var blocks : UInt32 = 0
+        (self as NSData).getBytes(&blocks, length: position)
+        return NSSwapLittleIntToHost(blocks)
+    }
+
+    func readUInt8(_ position : Int) -> UInt8 {
+        var blocks : UInt8 = 0
+        (self as NSData).getBytes(&blocks, length: position)
+        return blocks
+    }
+}
